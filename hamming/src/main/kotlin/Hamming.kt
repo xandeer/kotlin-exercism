1b371b665a8b4119d70aec7feb4d753f72ee61a6
Kevin Du <kkxandeer@gmail.com>
2022-04-20 11:09:52 +0800

    feat: learnt require, zip, indices and count

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,6 +1,8 @@
 object Hamming {
 
-    fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
-    }
+  fun compute(leftStrand: String, rightStrand: String): Int {
+    require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
+    // return leftStrand.indices.count { leftStrand[it] != rightStrand[it] }
+    return leftStrand.zip(rightStrand).count { it.first != it.second }
+  }
 }
