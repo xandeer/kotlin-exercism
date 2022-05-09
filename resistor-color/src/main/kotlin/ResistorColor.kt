d872ad51a73738526e8b5dd3f15b9e6954a1f7a8
Kevin Du <kkxandeer@gmail.com>
2022-05-09 08:42:29 +0800

    resolved resistor color

--- resistor-color/src/main/kotlin/ResistorColor.kt
+++ resistor-color/src/main/kotlin/ResistorColor.kt
@@ -1,11 +1,11 @@
 object ResistorColor {
 
-    fun colorCode(input: String): Int {
-        TODO("Implement this to complete the task")
-    }
+  fun colorCode(input: String): Int {
+    return colors().indexOf(input)
+  }
 
-    fun colors(): List<String> {
-        TODO("Implement this to complete the task")
-    }
+  fun colors(): List<String> {
+    return listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
+  }
 
 }
--- resistor-color/src/test/kotlin/ResistorColorTest.kt
+++ resistor-color/src/test/kotlin/ResistorColorTest.kt
@@ -3,18 +3,18 @@ import kotlin.test.Test
 
 class ResistorColorTest {
 
-    @Test
-    fun testBlackColorCode() = assertEquals(0, ResistorColor.colorCode("black"))
+  @Test
+  fun testBlackColorCode() = assertEquals(0, ResistorColor.colorCode("black"))
 
-    @Test
-    fun testWhiteColorCode() = assertEquals(9, ResistorColor.colorCode("white"))
+  @Test
+  fun testWhiteColorCode() = assertEquals(9, ResistorColor.colorCode("white"))
 
-    @Test
-    fun testOrangeColorCode() = assertEquals(3, ResistorColor.colorCode("orange"))
+  @Test
+  fun testOrangeColorCode() = assertEquals(3, ResistorColor.colorCode("orange"))
 
-    @Test
-    fun testColors() {
-        val expected = listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
-        assertEquals(expected, ResistorColor.colors())
-    }
+  @Test
+  fun testColors() {
+    val expected = listOf("black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white")
+    assertEquals(expected, ResistorColor.colors())
+  }
 }
