bf908188c70af441cec77ec7d99fcc11f12e6b5a
Kevin Du <kkxandeer@gmail.com>
2022-06-08 14:29:47 +0800

    review Int.and

---
+++ allergies/src/main/kotlin/Allergies.kt
@@ -0,0 +1,5 @@
+class Allergies(private val score: Int) {
+  fun getList(): List<Allergen> = Allergen.values().filter { isAllergicTo(it) }
+
+  fun isAllergicTo(allergen: Allergen) = score and allergen.score == allergen.score
+}
